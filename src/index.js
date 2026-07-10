831e18c0375d221b2940ac314385c33b45ad2d15
bk2zero1 <kodgias@gmail.com>
2026-07-10 11:44:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
 window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
@@ -7,18 +16,21 @@ window.onload = () => {
         redirect_uri: "https://oauth-master-class-theta-ten.vercel.app/token.html",
       },
       "https://oauth-master-class-theta-ten.vercel.app",
-      {
-        view: "button",
+{
         parentId: "buttonContainer",
-        buttonSize: "m",
-        buttonView: "main",
+        view: "button",
         buttonTheme: "light",
-        buttonBorderRadius: "0",
-        buttonIcon: "ya",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
       }
     )
       .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
-      .catch((error) => console.log("Обработка ошибки", error));
+      .then(async (data) => {
+        console.log("Сообщение с токеном(от кнопки): ", data);
+        const result = await fetchYandexData(data.access_token);
+        console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
+        authorize(result);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
   };
 };
