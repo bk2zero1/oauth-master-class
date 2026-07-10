c0d498a91668745d89d9f387ae51d23eccbe7b70
bk2zero1 <kodgias@gmail.com>
2026-07-10 13:16:40 +0300

    Update9 index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+// Авторизуем пользователя, используя инфу о нем.
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  display_name: displayName,
+}) => {
+  const avatarHtml = `<div class="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div class="name">${displayName}</div>`;
+
+  document.getElementById("auth").innerHTML = `${avatarHtml}${nameHtml}`;
+};
+
 // Делаем запрос за инфой о пользователе.
 const fetchYandexData = (token) =>
   fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
