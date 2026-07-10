6f34c7a374c87267936c9092f744a56d7e70eb98
bk2zero1 <kodgias@gmail.com>
2026-07-10 11:51:28 +0300

    Update2 index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const authorize = ({
+ default_avatar_id: defaultAvatarId,
+ display_name: displayName,
+}) => {
+ const avatarHtml = '<div class="avatar" style="background-image:url('https://avatars.nes.yandex.net/get-yapic/$(defaultAvatarId)/islands-middle')"></div>';
+ const nameHtml = '<div class="name">$(displayName)</div>';
+
+ document. getElementById("auth").innerHTML = ''$(avatar Html) $ (name Html)
+};
+
+const fetchYandexData = (token) =>
+ fetch('https://login.yandex.ru/info?format=json&oauth_token=$(token)').then(
+ (res) => res. json()
+ );
+
 window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
@@ -8,23 +23,20 @@ window.onload = () => {
       },
       "https://oauth-master-class-theta-ten.vercel.app",
 {
-                  view: 'button',
-                  parentId: 'container',
-                  buttonView: 'main',
-                  buttonTheme: 'light',
-                  buttonSize: 'm',
-                  buttonBorderRadius: 0
-               }
-            )
-            .then(function(result) {
-               return result.handler()
-            })
-            .then(function(data) {
-               console.log('Сообщение с токеном: ', data);
-               document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
-            })
-            .catch(function(error) {
-               console.log('Что-то пошло не так: ', error);
-               document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
-            });
-};
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+ };
