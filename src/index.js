a02200c913a825326ec8ec5be0a2f0851e7f25ec
bk2zero1 <kodgias@gmail.com>
2026-07-10 13:03:02 +0300

    Update8 index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,55 @@
-const authorize = ({
- default_avatar_id: defaultAvatarId,
- display_name: displayName,
-}) => {
- const avatarHtml = '<div class="avatar" style="background-image:url('https://avatars.nes.yandex.net/get-yapic/$(defaultAvatarId)/islands-middle')"></div>';
- const nameHtml = '<div class="name">$(displayName)</div>';
-
- document.getElementById("auth").innerHTML = ''$(avatar Html) $ (name Html)
-};
-
+// Делаем запрос за инфой о пользователе.
 const fetchYandexData = (token) =>
- fetch('https://login.yandex.ru/info?format=json&oauth_token=$(token)').then(
- (res) => res. json()
- );
+  fetch(`https://login.yandex.ru/info?format=json&oauth_token=${token}`).then(
+    (res) => res.json()
+  );
 
-   window.onload = function() {
-      window.YaAuthSuggest.init({
+window.onload = () => {
+  document.getElementById("suggest").onclick = () => {
+   YaAuthSuggest.init({
+         client_id: '0ab74019e8b84a7cb3d4e05d0f101130',
+         response_type: 'token',
+         redirect_uri: "https://oauth-master-class-theta-ten.vercel.app/token.html",
+      },
+      "https://oauth-master-class-theta-ten.vercel.app"
+                   
+   )
+ .then(({ handler }) => handler())
+      .then(async (data) => {
+        const result = await fetchYandexData(data.access_token);
+
+        authorize(result);
+
+        console.log(result, data);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
+   document.getElementById("button").onclick = () => {
+    window.YaAuthSuggest.init(
+      {
         client_id: "0ab74019e8b84a7cb3d4e05d0f101130",
         response_type: "token",
         redirect_uri: "https://oauth-master-class-theta-ten.vercel.app/token.html",
       },
-      "https://oauth-master-class-theta-ten.vercel.app", {
-                  view: 'button',
-                  parentId: 'container',
-                  buttonView: 'main',
-                  buttonTheme: 'light',
-                  buttonSize: 'm',
-                  buttonBorderRadius: 0
-               }
-            )
-            .then(function(result) {
-               return result.handler()
-            })
-            .then(function(data) {
-               console.log('Сообщение с токеном: ', data);
-               document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
-            })
-            .catch(function(error) {
-               console.log('Что-то пошло не так: ', error);
-               document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(error)}`;
-            });
-      };
+      "https://oauth-master-class-theta-ten.vercel.app",
+      {
+        parentId: "buttonContainer",
+        view: "button",
+        buttonTheme: "light",
+        buttonSize: "xs",
+        buttonBorderRadius: 20,
+      }
+    )
+      .then(({ handler }) => handler())
+      .then(async (data) => {
+        console.log("Сообщение с токеном(от кнопки): ", data);
+
+        const result = await fetchYandexData(data.access_token);
+
+        console.log("Сообщение с ответом Яндекса(от кнопки): ", result);
+
+        authorize(result);
+      })
+      .catch((error) => console.log("Что-то пошло не так: ", error));
+  };
+};
